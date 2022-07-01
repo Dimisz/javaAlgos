7e6f8e3cf69028a224f05cfca676c84a99150725
Vladimir Solovyov <leilyngale@icloud.com>
2022-07-01 11:11:45 +0300

    Recursion challenges

--- recursion/src/Power.java
+++ recursion/src/Power.java
@@ -1,12 +1,12 @@
 public class Power {
 
     public static void main(String[] args) {
-        System.out.println(recursivePower(-2, 3));
+        System.out.println(recursivePower(2, 3));
     }
 
-    static int recursivePower(int num, int power){
-        if(power == 0) return 0;
-        if(power == 1) return num;
-        return recursivePower(num, --power) * num;
+    static int recursivePower(int base, int exponent){
+        if(exponent == 0) return 1;
+        if(exponent < 0) return -1; // only calculating positive exponentials
+        return recursivePower(base, --exponent) * base;
     }
 }
